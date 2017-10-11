3d4f16cb28d032a3839a2dcfee096a0cdf108c47
proti <proti@protis-MacBook-Pro.local>
2017-10-11 12:32:42 +0200

    Making tests more automatic

--- src/test.js
+++ src/test.js
@@ -1,5 +1,4 @@
 const RomanNumber = require('./romanNumber');
-const testChars = [null, '', 0, 1, 3, 4, 5, 'I', 'III', 'IIII', 'IV', 'V', 1968, '173', 2999, 3000, 10000, 'CXXIX', 'C1X', 'error', 'MDLXXXII', 'MMLXXX', 'MMMMCMXCIX', 'MMMMDMXCIX'];
 
 const isItNull = (num) => num === new RomanNumber(null).toString();
 const isItEmptyStr = (num) => num === new RomanNumber('').toString();
@@ -16,15 +15,23 @@ const isItFiveArabic = (num) => num === new RomanNumber('V').toInt();
 const isIt1968 = (num) => num === new RomanNumber(1968).toString();
 const isIt173 = (num) => num === new RomanNumber('173').toString();
 const isIt2999 = (num) => num === new RomanNumber(2999).toString();
-const isIt3000= (num) => num === new RomanNumber(3000).toString();
-const isIt10000= (num) => num === new RomanNumber(10000).toString();
-const isIt129Arabic= (num) => num === new RomanNumber('CXXIX').toInt();
+const isIt3000 = (num) => num === new RomanNumber(3000).toString();
+const isIt10000 = (num) => num === new RomanNumber(10000).toString();
+const isIt129Arabic = (num) => num === new RomanNumber('CXXIX').toInt();
 const isItInValid = (num) => num === new RomanNumber('C1X').toString();
-const isItInValidStr= (num) => num === new RomanNumber('error').toString();
+const isItInValidStrError = (num) => num === new RomanNumber('error').toString();
 const isIt1582 = (num) => num === new RomanNumber('MDLXXXII').toInt();
 const isIt2080 = (num) => num === new RomanNumber('MMLXXX').toInt();
 const isItValidRomanNumber = (num) => num === new RomanNumber('MMMMCMXCIX').toInt();
 const isItValidRomanNumber2 = (num) => num === new RomanNumber('MMMMDMXCIX').toInt();
+
+const funcArr = [
+    [null, isItNull], ['', isItEmptyStr], [0, isItZero], [1, isItOneRoman], [3, isItThreeRoman], [4, isItFourRoman], [5, isItFiveRoman],
+    ['I', isItOneArabic], ['III', isItThreeArabic], ['IIII', isItInValidFourArabic], ['IV', isItFourArabic], ['V', isItFiveArabic],
+    [1968, isIt1968], ['173', isIt173], [2999, isIt2999], [3000, isIt3000], [10000, isIt10000], ['CXXIX', isIt129Arabic],
+    ['C1X', isItInValid], ['error', isItInValidStrError], ['MDLXXXII', isIt1582], ['MMLXXX', isIt2080],
+    ['MMMMCMXCIX', isItValidRomanNumber], ['MMMMDMXCIX', isItValidRomanNumber2]
+];
 
 testRoman = () => {
     console.log('testing...');
